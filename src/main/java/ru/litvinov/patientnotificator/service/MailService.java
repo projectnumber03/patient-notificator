0dbd66ae9ff06be162ccae6548868af335bbaaf1
sdv <abarakus@bk.ru>
2023-08-25 15:32:03 +0300

    added mail service

---
+++ src/main/java/ru/litvinov/patientnotificator/service/MailService.java
@@ -0,0 +1,40 @@
+package ru.litvinov.patientnotificator.service;
+
+import jakarta.mail.MessagingException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    @NonNull
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    @Value("${spring.mail.to}")
+    private String to;
+
+    public void sendMail(final String text, final String subject) {
+        try {
+            final var message = mailSender.createMimeMessage();
+            final var helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setFrom(from);
+            helper.setTo(to.split(","));
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            log.error("unable to send email \"{}\" email to {}", text, to);
+        }
+    }
+
+}
